9d557f0129cb465ab0331ff5bf49ef0daa058a99
Stefano Lapi <166375685+SteLaps@users.noreply.github.com>
2026-06-17 19:02:37 +0200

    Definit il file catalogo\admin.py per l'interfaccia admin di Django per Category e Product

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','slug')
+    prepopulated_fields = {'slug': ('name',)}  #Django crea lo slug basandosi sul nome
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'available', 'created_at')
+    list_filter = ('available', 'category')
+    list_editable = ('price', 'stock', 'available')  #possibilità di modificare la lista
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'description')
